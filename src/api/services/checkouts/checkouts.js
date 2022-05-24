c1a5be86661fdbb522b1d17ea244834572e0ef68
chris <chrsvdmrw@gmail.com>
2022-05-24 17:20:59 +0200

    fix syntax error

--- src/api/services/checkouts/checkouts.js
+++ src/api/services/checkouts/checkouts.js
@@ -2,38 +2,26 @@ import { logger } from '../../../web/lib'
 import { stripe } from '../../lib'
 
 export const checkout = async () => {
-  // eslint-disable-next-line camelcase
-  // const line_items = cart.map((product) => ({
-  //   price: product.id,
-  //   quantity: product.quantity,
-  // }))
-  logger("hi there")
-  return {
-    id: "24234234xcxds",
-    customerId: "cus00001",
-    customerEmail: 'nyman@gmail.com',
-    customerName: 'chris',
-    customerSignedUp: false
-  }
-  // const line_items = [
-  //   {
-  //     price: price_1Kb1YlHMAJHtnk9iwZZxLJjp,
-  //     quantity: 1
-  //   }, {
-  //     price: price_1Kb1YjHMAJHtnk9im9R7zlKO,
-  //     quantity: 2
-  //   }
-  // ]
+  
+  const line_items = [
+    {
+      price: "price_1Kb1YlHMAJHtnk9iwZZxLJjp",
+      quantity: 1
+    }, {
+      price: "price_1Kb1YjHMAJHtnk9im9R7zlKO",
+      quantity: 2
+    }
+  ]
 
-  // console.log('sup there')
+  const session = await stripe.checkout.sessions.create({
+    // See https://stripe.com/docs/payments/checkout/custom-success-page#modify-success-url.
+    success_url: `http://localhost:8910/success?sessionId={CHECKOUT_SESSION_ID}`,
+    cancel_url: `http://localhost:8910/failure`,
+    // eslint-disable-next-line camelcase
+    line_items,
+    mode: 'payment',
+    payment_method_types: ['card']
+  })
 
-  // return stripe.checkout.sessions.create({
-  //   // See https://stripe.com/docs/payments/checkout/custom-success-page#modify-success-url.
-  //   success_url: `http://localhost:8910/success?sessionId={CHECKOUT_SESSION_ID}`,
-  //   cancel_url: `http://localhost:8910/failure`,
-  //   // eslint-disable-next-line camelcase
-  //   line_items,
-  //   mode: 'payment',
-  //   payment_method_types: ['card']
-  // })
+  return session
 }
