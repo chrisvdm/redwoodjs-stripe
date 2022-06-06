290b23fcafbcd7271c0af91a8a884d30e8a8bd7d
chris <chrsvdmrw@gmail.com>
2022-06-06 11:21:16 +0200

    clean up

--- src/web/components/StripeProductCard/StripeProductCard.js
+++ src/web/components/StripeProductCard/StripeProductCard.js
@@ -1,22 +1,9 @@
-import { StripeButton } from "../StripeButton/StripeButton"
+import '../styles.css'
 
 export const StripeProductCard = ({ name, children = '' }) => {
-    const handleOnStripeButtonAddToCartClick = () => {
-        console.log(`Adding product id ${id} to`)
-    }
-    
-    return (
-    <div style={productCardStyle}>
+return (
+    <div className='stripe-product-card'>
       <h3>{name}</h3>
       {children}
     </div>
 )}
-
-const productCardStyle = {
-    padding: '10px 20px',
-  borderRadius: '5px',
-  backgroundColor: '#fff',
-  color: '#3a444a',
-  boxShadow: '2px 2px #dcdbdb',
-    fontStyle: 'normal',
-}
