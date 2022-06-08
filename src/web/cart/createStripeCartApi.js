9df11084d6bebc88c5db4482ca923eed50e6c895
chris <chrsvdmrw@gmail.com>
2022-06-08 13:41:44 +0200

    Update checkout to use cart

--- src/web/cart/createStripeCartApi.js
+++ src/web/cart/createStripeCartApi.js
@@ -3,9 +3,40 @@
 export const createStripeCartApi = (cart, setCart) => ({
   cart,
   addToCart: (item) => {
-    setCart([...cart, item])
+    const prevCart = [...cart] 
+    const itemIndex = findItemIndexByID(item, prevCart)
+    let newCart = []
+
+    if (itemIndex >= 0) {
+      // replace obj with new object
+      newCart = prevCart
+      newCart.splice(itemIndex, 1, {
+          id: item,
+          quantity: prevCart[itemIndex].quantity + 1
+        })
+    } else {
+      newCart = [
+        ...cart,
+        {
+          id: item,
+          quantity: 1
+        }
+      ]
+    }
+
+    setCart(newCart)
   },
   clearCart: () => {
     setCart([])
   },
 })
+
+const findItemIndexByID = (key, inputArray) => {
+  for (let i=0; i < inputArray.length; i++) {
+      if (inputArray[i].id === key) {
+          return i;
+      } else {
+        return -1;
+      }
+  }
+}
