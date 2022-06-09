fc76c422de8618a2aa4e22394ae18ba51245f504
chris <chrsvdmrw@gmail.com>
2022-06-09 11:26:31 +0200

    syntax error

--- src/api/services/checkouts/checkouts.js
+++ src/api/services/checkouts/checkouts.js
@@ -18,7 +18,7 @@ export const createStripeCheckoutSession = async (cart) => {
   const line_items = cart.map(product => ({
     price: product.id,
     quantity: product.quantity
-    }))
+  }))
 
   // TODO: Pass custom payload
   const session = await stripe.checkout.sessions.create({
@@ -26,7 +26,7 @@ export const createStripeCheckoutSession = async (cart) => {
     success_url: `http://localhost:8910/success?sessionId={CHECKOUT_SESSION_ID}`,
     cancel_url: `http://localhost:8910/failure`,
     // eslint-disable-next-line camelcase
-    line_items,
+    line_items: line_items,
     mode: 'payment',
     payment_method_types: ['card']
   })
