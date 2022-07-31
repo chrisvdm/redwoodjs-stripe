9228ed1d9261a1cb30c7d0f736bf08eedad4cc77
chris <chrsvdmrw@gmail.com>
2022-07-31 12:23:04 +0200

    create portal session service

--- src/api/services/customerPortal/customerPortal.js
+++ src/api/services/customerPortal/customerPortal.js
@@ -1,7 +1,8 @@
 import { stripe } from '../../lib'
 
 export const createStripeCustomerPortalSession = (payload) => {
-    
+    const session = await stripe.billingPortal.sessions.create(payload);
+    return session
 }
 
 export const configureStripeCustomerPortal = () => {
