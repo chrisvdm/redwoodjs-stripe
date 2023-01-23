9f7d81253c3b6642b03700874d280f2ccaf22be8
chris <chrsvdmrw@gmail.com>
2023-01-23 14:30:45 +0200

    white space addition

--- src/web/provider/StripeProvider/StripeProvider.js
+++ src/web/provider/StripeProvider/StripeProvider.js
@@ -22,12 +22,11 @@ export const StripeProvider = ({
 }) => {
   const [cart, setCart] = useState([])
   const [stripeCustomer, setCustomer] = useState(null)
-
   useStripeCustomerFetchOrCreate(id, search, create, setCustomer)
   // Returns a fn that returns a promise when stripeCustomer is null
   // else returns resolved stripeCustomer value
   const whenCustomerResolved = useWatcher(stripeCustomer, isNotNull)
-
+  
   const waitForCustomer = async () => {
     // Check that we have what we need to either fetch(search) for and create a Stripe Customer
     if (search !== '' && Object.keys(create).length > 0) {
