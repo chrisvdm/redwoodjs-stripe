a4a8a76a8f55eda9aa48db1d0797f6de73372366
chris <chrsvdmrw@gmail.com>
2023-06-15 09:59:29 +0200

    Remove unused components

--- web/src/components/StripeProductCard/StripeProductCard.js
+++
@@ -1,9 +0,0 @@
-import '../styles.css'
-
-export const StripeProductCard = ({ name, children = '' }) => {
-return (
-    <div className='stripe-product-card'>
-      <h3>{name}</h3>
-      {children}
-    </div>
-)}
--- web/src/components/index.js
+++
@@ -1,2 +0,0 @@
-export { StripeProductCard } from './StripeProductCard/StripeProductCard'
-export { StripeButton } from './StripeButton/StripeButton'
