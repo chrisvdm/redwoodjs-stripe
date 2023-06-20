4b302ed51fc3386adc75fc3f4d97ae2dc99f56af
chris <chrsvdmrw@gmail.com>
2023-06-20 12:23:16 +0200

    Update webhook test template

--- cli/templates/api/src/functions/stripeWebhook/stripeWebhook.test.js
+++ cli/templates/api/src/functions/stripeWebhook/stripeWebhook.test.js
@@ -1,41 +1,13 @@
-import { mockHttpEvent } from '@redwoodjs/testing/api'
-
-import { stripe } from 'src/lib/stripe'
-
-import { handler } from './stripeWebhook'
+import {
+  handleStripeWebhooks,
+  generateStripeMockEvent,
+} from '@redwoodjs-stripe/api'
 
 describe('stripeWebhooks function', () => {
   it('Should respond with 200', async () => {
-    /**
-     * Copied from Stripe's tests: {@link https://github.com/stripe/stripe-node/blob/master/test/Webhook.spec.js#L8-L12}
-     */
-    const payload = JSON.stringify(
-      {
-        id: 'evt_test_webhook',
-        object: 'event',
-      },
-      null,
-      2
-    )
+    const mockStripeEvent = generateStripeMockEvent()
 
-    process.env.STRIPE_WEBHOOK_SK = 'whsec_test_secret'
-
-    /**
-     * @see {@link https://github.com/stripe/stripe-node/blob/master/README.md#testing-webhook-signing}
-     */
-    const header = stripe.webhooks.generateTestHeaderString({
-      payload,
-      secret: process.env.STRIPE_WEBHOOK_SK,
-    })
-
-    const httpEvent = mockHttpEvent({
-      body: payload,
-      headers: {
-        'stripe-signature': header,
-      },
-    })
-
-    const response = await handler(httpEvent, null)
+    const response = await handleStripeWebhooks(mockStripeEvent, {}, {}, false)
 
     expect(response.statusCode).toBe(200)
   })
