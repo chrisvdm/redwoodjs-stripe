dd885e4a83a1157bb360d23df83daaddce9ab85d
chris <chrsvdmrw@gmail.com>
2023-06-21 17:06:59 +0200

    add missing prompts module

--- cli/src/setup.js
+++ cli/src/setup.js
@@ -4,6 +4,7 @@ const path = require('node:path');
 const util = require('node:util');
 const exec = util.promisify(require('node:child_process').exec);
 
+const prompts = require('prompts');
 const fs = require('fs-extra');
 const { Listr } = require('listr2');
 const Stripe = require('stripe');
@@ -87,6 +88,30 @@ const copyTemplateFiles = async (options) => {
 
 const shouldSkip = (options, step) => [...(options.skip || [])].includes(step);
 
+const importSchemasAndServices = async () => {
+  const graphQLFile = './api/src/functions/graphql.js'
+  fs.readFile(graphQLFile, 'utf8', (err, data) => {
+    
+  if (err) {
+    return console.log(err);
+  }
+    
+    // Replace services and sdls
+    const result = data.replace(/import sdls from/g, 'import * as rwSdls from');
+    const result2 = result.replace(/import services from/g, 'import * as rwServices from');
+
+    // import plugin services at top of file
+    
+
+    // create new sdls and schemas objects
+    
+
+  fs.writeFile(graphQLFile, result2, 'utf8', (err) => {
+     if (err) return console.log(err);
+  });
+});
+}
+
 const scaffold = async (options) => {
   if (!shouldSkip(options, 'pluginDeps')) {
     await exec('yarn add @redwoodjs-stripe/web', { cwd: path.join(options.dir, 'web') });
@@ -121,10 +146,14 @@ const setup = async (initialOptions) => {
       title: 'Adding dummy products',
       task: () => addDummyProducts(options),
     },
+    // {
+    //   title: 'Scaffolding out project files',
+    //   task: () => scaffold(options),
+    // },
     {
-      title: 'Scaffolding out project files',
-      task: () => scaffold(options),
-    },
+      title: 'Importing Schemas and Services',
+      task: () => importSchemasAndServices()
+    }
   ].filter(Boolean);
 
   try {
