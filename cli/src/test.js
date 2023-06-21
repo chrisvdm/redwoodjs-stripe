e3e343ed6afcdfca0a0b53a405afe730cb744dbf
chris <chrsvdmrw@gmail.com>
2023-06-21 20:14:07 +0200

    refactor test file

--- cli/src/test.js
+++ cli/src/test.js
@@ -2,6 +2,7 @@
 
 const util = require('node:util');
 const fs = require('fs-extra');
+const { forEach } = require('core-js/core/array');
 
 (async () => {
     const graphQLFile = './api/src/functions/graphql.js'
@@ -11,39 +12,46 @@ const fs = require('fs-extra');
                 regex: /import sdls from/g,
                 replacement:'import * as rwSdls from'
             }, {
-                regex: /import schemas from/g,
-                replacement:'import * as rwSchemas from'
+                regex: /import services from/g,
+                replacement:'import * as rwServices from'
             }
         ],
         pluginImport: 'import { stripeSchemas, stripeServices } from \'@redwoodjs-stripe/api\'',
-        apiServicesAndSchemas: [
+        apiGraphQL: [
             'const services = { ...rwServices, ...stripeServices }',
             'const sdls = { ...rwSdls, ...stripeSchemas }'
         ]
     }
     fs.readFile(graphQLFile, 'utf8', (err, data) => {
-    
+        let updatedFile = ''
         if (err) {
             return console.log(err);
         }
     
         // Replace services and sdls
-        const result = data.replace(/import sdls from/g, 'import * as rwSdls from');
-        const result2 = result.replace(/import services from/g, 'import * as rwServices from');
+        updatedFile = text.rwImports.reduce((resultData, imp) => {
+            const newData = resultData.replace(imp.regex, imp.replacement)
+            return newData
+        }, data)
+
 
         // import plugin services at top of file
-        const fileWithImports = ('import { stripeSchemas, stripeServices } from \'@redwoodjs-stripe/api\'').concat('\n\n', result2)
+        updatedFile = (text.pluginImport).concat('\n\n', result2)
     
         // create new sdls and schemas objects
-        const reassignedRWGraphQL = "import * as rwServices from \'src/services/**/*.{js,ts}\'"
-        const SchemaStart = fileWithImports.indexOf(reassignedRWGraphQL) + reassignedRWGraphQL.length
-        const firstSection = fileWithImports.slice(0, SchemaStart)
-        const endSection = fileWithImports.slice(SchemaStart)
-        const completeFile = firstSection.concat(`\n\nconst services = { ...rwServices, ...stripeServices }\nconst sdls = { ...rwSdls, ...stripeSchemas }`, endSection)
-    
-        fs.writeFile(graphQLFile, completeFile, 'utf8', (err) => {
+        updatedFile = insertLinesAfter("import * as rwServices from \'src/services/**/*.{js,ts}\'", text.apiGraphQL, fileWithImports )
+        
+        fs.writeFile(graphQLFile, updatedFile, 'utf8', (err) => {
             if (err) return console.log(err);
         });
       
     })
 })()
+
+const insertLinesAfter = (txt, replacementArray, fileData) => {
+    const SchemaStart = fileData.indexOf(txt) + txt.length
+    const firstSection = fileData.slice(0, SchemaStart)
+    const endSection = fileData.slice(SchemaStart)
+    const insertedPar =replacementArray.join('\n')
+    return firstSection.concat(`\n${insertedPar}\n`, endSection)   
+}
