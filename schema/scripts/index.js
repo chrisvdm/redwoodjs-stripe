fb4407249b5a1c3923cd0d30177d5e523f33f922
chris <chrsvdmrw@gmail.com>
2023-07-18 19:40:54 +0200

    exploring avenues

--- schema/scripts/index.js
+++ schema/scripts/index.js
@@ -1,5 +1,28 @@
-const main = () => {
-    console.log("bruh")
+const fetch = require('node-fetch');
+
+const main = async () => {
+    try {
+        // Fetch Stripe openAPI spec
+        const openAPIBlob = await fetch('https://raw.githubusercontent.com/stripe/openapi/master/openapi/spec3.json');
+        const openAPI = JSON.parse(await openAPIBlob.text());
+
+        // Retrieve schema information
+        const openAPISchema = openAPI.components.schemas
+
+        // Specify active (used) Stripe Objects
+        // TODO: Empty array leads to all objects being processed
+        const activeObjects = ["checkout.session", "subscription"]
+
+        // Create new array of openapi schema objects
+        let activeObjectsOpenAPI = []
+        activeObjects.forEach(obj => {
+            activeObjectsOpenAPI.push(openAPISchema[obj])
+        });
+        console.log(activeObjectsOpenAPI)
+
+  } catch (err) {
+    console.log('fetch error', err);
+  }
 }
 
 main()
