f49456232af57768a53d5106fbe8c1eb5e2f5b9e
chris <chrsvdmrw@gmail.com>
2023-07-24 15:46:24 +0200

    get basic types

--- schema/scripts/index.js
+++ schema/scripts/index.js
@@ -1,5 +1,5 @@
 const fetch = require('node-fetch');
-const { buildSchema } = require("graphql")
+const graphql = require("graphql")
 
 const main = async () => {
     try {
@@ -29,21 +29,79 @@ const main = async () => {
       //    2nd iteraction: Build from sdl
 
       // Define the Query type
-      const queryType = new graphql.GraphQLObjectType({
-        name: "Query",
-        fields: {
-          stripeCustomer: {
-            type: getGraphQLObjectType('stripeCustomer'),
-            args: {
-              id: { type: graphql.GraphQLID },
-            }
-          },
-        },
-      })
+      // const queryType = new graphql.GraphQLObjectType({
+      //   name: "Query",
+      //   fields: {
+      //     stripeCustomer: {
+      //       type: getGraphQLObjectType('customer', context),
+      //       args: {
+      //         id: { type: graphql.GraphQLID },
+      //       }
+      //     },
+      //   },
+      // })
+
+      const getGraphQLType = ({ type, isRequired}) => {
+        switch (type) {
+          case 'string':
+            return graphql.GraphQLString
+          case 'integer':
+            return graphql.GraphQLInt
+          case 'boolean':
+            return graphql.GraphQLBoolean
+          case 'id':
+            return graphql.GraphQLID
+        }
+      }
 
       const getGraphQLObjectType = (name) => {
+        let objectFieldsGraphQLType = {}
+        // TODO sanitise name string
+        // - remove 'stripe' prefix
 
+        // get data from openAPISchema
+        const ugh = openAPISchema[name]
+        const { properties, required, title } = ugh
+        const expandable = ugh['x-expandableFields']
+        
+        // Goes through each property and return corresponding GraphQL Types
+        Object.keys(properties).forEach(name => {
+          const prop = properties[name]
+          let propGraphQLType = ''
+
+          // determine whether prop is scalar
+          const isRef = Object.hasOwn(prop, `\$ref`)
+          const isUnion = Object.hasOwn(prop, 'AnyOf')
+          const isEnum = Object.hasOwn(prop, 'enum')
+          const isHash = prop.type === 'object' && !Object.hasOwn(prop, 'properties') && !expandable.includes(name)
+          const isObject = !isHash && prop.type === 'object'
+          
+          const isRequired = !required.includes(name)
+
+          const propValues = {
+            objectName: name,
+            ...prop,
+            isRequired,
+          }
+
+          if (!isRef && !isUnion && !isHash && !isObject && !isEnum ) {
+             propGraphQLType = getGraphQLType(propValues)
+          }
+
+          objectFieldsGraphQLType[name]= {
+            type: propGraphQLType
+          }
+
+        });
+
+        // Construct object type
+        return new graphql.GraphQLObjectType({
+          name: `stripe${title}`,
+          fields: {...objectFieldsGraphQLType},
+        })
       }
+
+      console.log(getGraphQLObjectType('checkout.session'))
 
       // 2. Build out types defined in QueryType
       //      1st iteration: Build first level properties
@@ -53,8 +111,6 @@ const main = async () => {
       // var schema = new graphql.GraphQLSchema({ query: queryType })
 
       // console.log(schema)
-
-
 
   } catch (err) {
     console.log('fetch error', err);
