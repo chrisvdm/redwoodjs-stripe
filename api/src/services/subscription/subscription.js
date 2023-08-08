8fccf58d46487c43b6ce25a3ccb5bac412c268ea
chris <chrsvdmrw@gmail.com>
2023-08-08 11:40:56 +0200

    explode parameters

--- api/src/services/subscription/subscription.js
+++ api/src/services/subscription/subscription.js
@@ -1,6 +1,7 @@
-import { stripe } from '../../lib'
+import { stripe, logger } from '../../lib'
+
 
 export const listStripeSubscriptions = async ({ listParams, addProps }) => {
-  const subscriptions = await stripe.subscriptions.list(listParams, addProps);
+  const subscriptions = await stripe.subscriptions.list({...listParams}, {...addProps});
   return subscriptions.data
 }
