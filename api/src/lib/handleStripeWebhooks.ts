82990e1e8ef6dd82e1f38aec9390ef91494bb64b
Justin van der Merwe <justnvdm@gmail.com>
2024-07-01 15:15:52 +0200

    handle stripe webhook

--- api/src/lib/handleStripeWebhooks.ts
+++ api/src/lib/handleStripeWebhooks.ts
@@ -1,32 +1,59 @@
-export const handleStripeWebhooks = async (
+import type { APIGatewayEvent, Context } from "aws-lambda";
+import type { Stripe } from "stripe";
+import { stripe } from "./stripe";
+import { nonNilAssertionError } from "./nonNilAssertionError";
+
+type Webhook = (
+  event: Stripe.Event,
+  context: Context,
+) => unknown | Promise<unknown>;
+
+type Webhooks = Record<Stripe.Event["type"], Webhook>;
+
+interface HandleStripeWebhooksProps {
+  event: APIGatewayEvent;
+  context: Context;
+  webhooks?: Webhooks;
+  secure?: boolean;
+}
+
+export const handleStripeWebhooks = async ({
   event,
   context,
-  webhooksObj = {},
+  webhooks = {} as Webhooks,
   secure = true,
-) => {
+}: HandleStripeWebhooksProps) => {
   if (secure) {
     const endpointSecret = process.env.STRIPE_WEBHOOK_KEY;
 
     if (!endpointSecret) {
-      throw new Error(`The Stripe webhook secret key isn't set`);
+      throw nonNilAssertionError("handleStripeWebhooks:webhookKey", {
+        endpointSecret,
+      });
     }
 
     const signature = event.headers["stripe-signature"];
 
+    if (!signature) {
+      throw nonNilAssertionError("handleStripeWebhooks:signature", {
+        signature,
+      });
+    }
+
     // For Vercel deploys, events are based64 encoded
     const parsedBody = event.isBase64Encoded
-      ? Buffer.from(event.body, "base64").toString("utf-8")
+      ? Buffer.from(event.body ?? "", "base64").toString("utf-8")
       : event.body;
 
     const stripeEvent = stripe.webhooks.constructEvent(
-      parsedBody,
+      parsedBody ?? "",
       signature,
       endpointSecret,
     );
 
     // Find event type in webhookObject and execute function for the event.
-    if (typeof webhooksObj[stripeEvent.type] !== "undefined") {
-      await webhooksObj[stripeEvent.type](stripeEvent, context);
+    if (typeof webhooks[stripeEvent.type] !== "undefined") {
+      await webhooks[stripeEvent.type](stripeEvent, context);
     }
 
     return {
@@ -38,13 +65,11 @@ export const handleStripeWebhooks = async (
       throw new Error("Stripe webhooks must be secure in production");
     }
 
-    const unverifiedStripeEvent = JSON.parse(event.body);
+    const unverifiedStripeEvent = JSON.parse(event.body ?? "");
+    const eventType = unverifiedStripeEvent.type as Stripe.Event["type"];
 
-    if (typeof webhooksObj[unverifiedStripeEvent.type] !== "undefined") {
-      await webhooksObj[unverifiedStripeEvent.type](
-        unverifiedStripeEvent,
-        context,
-      );
+    if (typeof webhooks[eventType] !== "undefined") {
+      await webhooks[eventType](unverifiedStripeEvent, context);
     }
 
     return {
