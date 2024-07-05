0b59788042215b76e27ccbab2d4972bc2755d16d
Justin van der Merwe <justinvderm@gmail.com>
2024-07-05 21:41:39 +0200

    createStripeCustomerPortalSession

--- api/src/services/customerPortal/customerPortal.ts
+++ api/src/services/customerPortal/customerPortal.ts
@@ -1,21 +1,53 @@
-import { stripe } from "../../lib.js";
+import type { Stripe } from "stripe";
+import type {
+  MutationCreateStripeCustomerPortalConfigArgs,
+  MutationCreateStripeCustomerPortalSessionArgs,
+  MutationCreateStripeCustomerPortalSessionSkipAuthArgs,
+  QueryListStripeCustomerPortalConfigArgs,
+} from "../../generated/graphql.js";
+import { deepOmitNils } from "../../lib/deepOmitNils.js";
+import { nonNilAssertionError } from "../../lib/nonNilAssertionError.js";
+import { stripe } from "../../lib/stripe.js";
 
-export const createStripeCustomerPortalSession = async ({ data }) => {
-  const session = await stripe.billingPortal.sessions.create(data);
+export const createStripeCustomerPortalSession = async ({
+  data: inputData,
+}: MutationCreateStripeCustomerPortalSessionArgs) => {
+  const data = deepOmitNils(inputData)
+
+  if (data?.customer == null) {
+    throw nonNilAssertionError(
+      "createStripeCustomerPortalSession:data",
+      data,
+    );
+  }
+
+  const params = {
+    ...data,
+    locale: data.locale as Stripe.BillingPortal.SessionCreateParams.Locale,
+  }
+
+  const session = await stripe.billingPortal.sessions.create(params);
+
   return session;
 };
 
-export const createStripeCustomerPortalSessionSkipAuth = async (payload) => {
+export const createStripeCustomerPortalSessionSkipAuth = async (
+  payload: MutationCreateStripeCustomerPortalSessionSkipAuthArgs,
+) => {
   const session = await createStripeCustomerPortalSession(payload);
   return session;
 };
 
-export const createStripeCustomerPortalConfig = async ({ data }) => {
+export const createStripeCustomerPortalConfig = async ({
+  data,
+}: MutationCreateStripeCustomerPortalConfigArgs) => {
   const config = await stripe.billingPortal.configurations.create(data);
   return config;
 };
 
-export const listStripeCustomerPortalConfig = async ({ params }) => {
+export const listStripeCustomerPortalConfig = async ({
+  params,
+}: QueryListStripeCustomerPortalConfigArgs) => {
   const configArray = await stripe.billingPortal.configurations.list(params);
   return configArray;
 };
